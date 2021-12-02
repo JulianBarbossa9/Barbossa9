a948036f8ccfab373d97ff345bd641dcef7ee5b4
JulianBarbossa9 <julianbarbossa@hotmail.com>
2021-12-02 18:56:04 -0500

    responsive menu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { MdClose, MdMenu } from 'react-icons/md'
 
 const NavMenuStyle = styled.div`
     position: fixed;
@@ -38,12 +39,65 @@ const NavMenuStyle = styled.div`
             color: var(--white);
         }
     }
+    .mobile-menu-icon{
+        position: absolute;
+        right: 3rem;
+        top:1rem;
+        width: 2rem;
+        cursor: pointer;
+        display: none;
+        outline: none;
+    }
+    .closeNavIcon{
+        display: none;
+
+    }
+    @media only screen and (max-width: 768px){
+        .mobile-menu-icon {
+            display: block;
+            width: 1.5rem;
+        }
+        .navItems{
+            --top:1rem;
+            transition: 0.3s ease transform;
+            background-color: var(--deep-dark);
+            padding: 2rem;
+            width: 90%;
+            max-width: 300px;
+            border-radius: 12px;
+            position: absolute;
+            right: 1rem;
+            top:var(--top);
+            .closeNavIcon {
+                display: block;
+                width: 1.5rem;
+                margin: 0 0 0 auto;
+                cursor: pointer;
+                *{
+                    pointer-events: none;
+                }
+            }
+            li{
+                display: block;
+                margin-bottom: 0.5rem;
+            }
+        }
+        
+    }
 `;
 
 const NavMenu = () => {
     return (  
         <NavMenuStyle>
-            <ul>
+            <div className="mobile-menu-icon">
+            <MdMenu />
+            </div>
+            <ul className="navItems">
+
+                <div className="closeNavIcon">
+                    <MdClose />
+                </div>
+
                 <li>
                     <NavLink to="/" exact> Home </NavLink>
                 </li>
